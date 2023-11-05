55c9049690b03ecb0841a4392956db0d5da50619
Salma Youssef <syoussef4444@gmail.com>
2023-11-05 19:06:49 +0200

    Error handling in Login view

--- lib/views/LoginView.dart
+++ lib/views/LoginView.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer'as devtools show log;
-
 import 'package:untitled/constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 
 class LoginView extends StatefulWidget {
@@ -16,22 +14,25 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+
   @override
   void initState() {
     _email = TextEditingController();
-    _password =TextEditingController();
+    _password = TextEditingController();
     super.initState();
   }
+
   @override
   void dispose() {
     _email.dispose();
     _password.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login',),),
+      appBar: AppBar(title: const Text('Login')),
       body: Column(
         children: [
           Container(
@@ -39,46 +40,60 @@ class _LoginViewState extends State<LoginView> {
           ),
           Container(
             padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0.0),
-            child: TextField( controller: _email,decoration: InputDecoration(border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-                hintText: 'Enter email',prefixIcon: const Icon(Icons.email)),
+            child: TextField(
+              controller: _email,
+              decoration: InputDecoration(border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+                  hintText: 'Enter email', prefixIcon: const Icon(Icons.email)),
               keyboardType: TextInputType.emailAddress,
               enableSuggestions: false,
               autocorrect: false,),
           ),
           Container(
             padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0.0),
-            child: TextField(controller: _password,
+            child: TextField(
+              controller: _password,
               obscureText: true,
               enableSuggestions: false,
               autocorrect: false,
               decoration: InputDecoration(border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(30),
               ),
-                hintText: 'Enter password',prefixIcon: const Icon(Icons.key),),
+                hintText: 'Enter password', prefixIcon: const Icon(Icons.key),),
             ),
           ),
-          TextButton(onPressed: ()async{
-            try {final email=_email.text;
-            final password=_password.text;
-            final userCredential =await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-            Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false);
-            }on FirebaseAuthException catch(e)
-            {
-              if(e.code=='INVALID_LOGIN_CREDENTIALS')
-              {
-                devtools.log('User not found');
-                devtools.log(e.code);
-              }
-            }
-          },child: const Text('Login'),),
-        TextButton(
-            onPressed: (){
-              Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
+          TextButton(onPressed: () async {
+            try {
+              final email = _email.text;
+              final password = _password.text;
+              await FirebaseAuth.instance
+                  .signInWithEmailAndPassword(email: email, password: password);
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                  notesRoute, (route) => false);
+            } on FirebaseAuthException catch (e) {
 
-        }, child: const Text('Not registered yet ? click here'))],
+              if (e.code == 'too-many-requests') {
+                await showErrorDialog( context,  'Wrong password',);
+              }
+              else if(e.code == 'INVALID_LOGIN_CREDENTIALS')
+                {await showErrorDialog( context,  'User not found',);}
+              else {
+                await showErrorDialog( context,  e.code);
+              }
+            } catch(e) {
+              await showErrorDialog( context,  e.toString());
+            }
+          }, child: const Text('Login'),),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    registerRoute, (route) => false);
+              }, child: const Text('Not registered yet ? click here'))
+        ],
       ),
     );
   }
 }
+
+
